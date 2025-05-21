15bc3c4cfd1895349adc04a60164208c59529880
LuisOtavi0 <luis-o-ss@hotmail.com>
2025-05-20 22:29:15 -0300

    Atualizacoes de linkagem

--- src/Controllers/AuthController.js
+++ src/Controllers/AuthController.js
@@ -16,7 +16,7 @@ class AuthController {
 
             const {senha: hashedSenha, ...usuario} = usuarioEncontrado.toObject();
 
-            const token = jwt.sign(usuario, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRE_IN});
+            const token = jwt.sign({usuario}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRE_IN});
 
             res.status(200).json({token});
         } catch (error) {
